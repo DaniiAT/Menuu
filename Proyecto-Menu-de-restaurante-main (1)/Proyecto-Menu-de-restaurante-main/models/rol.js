dc977e01acdb488c24f66d533daa7f5726ee96b0
DaniiAT <eiddatdani@gmail.com>
2024-11-24 21:31:13 -0300

    Update rol.js

--- Proyecto-Menu-de-restaurante-main (1)/Proyecto-Menu-de-restaurante-main/models/rol.js
+++ Proyecto-Menu-de-restaurante-main (1)/Proyecto-Menu-de-restaurante-main/models/rol.js
@@ -1,12 +1,1 @@
-import { Schema, model } from "mongoose";
 
-const RolSchema = Schema ({
-rol:{
-    type:String,
-    required: [true, "El rol es obligatorio"],
-}
-
-})
-
-export default model ("Role", RolSchema
-)
